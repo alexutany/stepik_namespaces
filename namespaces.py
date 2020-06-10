7f49391b372a4a8308a4762fea59aa713f216c8a
alexutany <42452949+alexutany@users.noreply.github.com>
2020-06-10 13:01:02 +0300

    Create namespaces.py

---
+++ namespaces.py
@@ -0,0 +1,62 @@
+requests = []
+
+namespaces = [{'name': 'global', 'parent': 'namespaces', 'children': []}]
+
+def parse_input(req):
+    
+    request = req.split()
+    method = request[0]
+    namespace = request[1]
+    var = request[2]
+
+    if method == 'create':
+        create(namespace, var)
+    if method == 'add':
+        add(namespace, var)
+    if method == 'get':
+        print(get(namespace, var))
+
+def create(namespace, parent):
+    
+    for item in namespaces:
+        if item['name'] == namespace:
+            item['children'].append(var)
+
+    element = {
+        'name': namespace,
+        'parent': parent,
+        'children': [] 
+        }
+    
+    namespaces.append(element)
+
+def add(namespace, var):
+    
+    for item in namespaces:
+        if item['name'] == namespace:
+            item['children'].append(var)
+    
+    element = {
+        'name': var,
+        'parent': namespace,
+        'children': [] 
+        }
+    
+    namespaces.append(element)
+
+def get(namespace, var):
+    
+    for item in namespaces:
+        if item['name'] == namespace:
+            if var in item['children']:
+                return item['name']
+            else:
+                rec = get(item['parent'], var) 
+                return rec
+
+q = int(input())
+for i in range(q):
+    requests.append(str(input()))
+
+for item in requests:
+    parse_input(item)
